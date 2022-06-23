2ed7212f1220035828a2e2a6bb4f79a6e44c9155
SaideepArikontham-MT <104340044+SaideepArikontham-MT@users.noreply.github.com>
2022-06-23 14:46:36 +0530

    updated

---
+++ notebooks/nb1.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+dbutils.fs.mount(
+  source = "wasbs://input@blb2006.blob.core.windows.net",
+  mount_point = "/mnt/repro",
+  extra_configs = {"fs.azure.account.key.blb2006.blob.core.windows.net":"Q5cucB7iHbmzEb7zxzdU6Y7V1T1/fo9RO5RLaXrlM6SZss00hfvKMKi3y2GYwSirtWMtJHqCE8Tk+ASt3ofitw=="})
+
+# COMMAND ----------
+
+df = spark.read.option("header", False).csv("/mnt/repro/country_data.csv")
+
+# COMMAND ----------
+
+df.show()
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+df.repartition(3).write.mode('overwrite').option("header", False).format("csv").save("/mnt/repro/country")
+
+# COMMAND ----------
+
